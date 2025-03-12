712e0a240dc4fd7897c5c3f1458836bf767ae49f
javadbat <javadbat@gmail.com>
2025-03-12 18:32:19 +0330

    chore: add storybook tests

---
+++ stories/JBNotification.stories.tsx
@@ -0,0 +1,13 @@
+import { JBNotificationTest } from "./samples/jb-notification-test";
+import type { Meta, StoryObj } from '@storybook/react';
+
+const meta: Meta<unknown> = {
+  title: "Example/JBNotification",
+  component: JBNotificationTest,
+};
+export default meta;
+type Story = StoryObj<typeof JBNotificationTest>;
+
+export const Normal: Story = {
+  args: {}
+};
