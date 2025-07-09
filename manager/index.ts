fe0f46edbf9990dd2ec86cc3f158088154b8769c
javadbat <javadbat@gmail.com>
2025-07-09 23:59:51 +0330

    update sample manager

---
+++ manager/index.ts
@@ -0,0 +1,46 @@
+import 'jb-notification';
+import 'jb-notification/wrapper';
+// eslint-disable-next-line no-duplicate-imports
+import {JBNotificationWrapperWebComponent} from 'jb-notification/wrapper';
+// eslint-disable-next-line no-duplicate-imports
+import { type JBNotificationWebComponent, type NotificationType } from 'jb-notification';
+// new messaging system
+class NotificationManager {
+  #wrapperDom:JBNotificationWrapperWebComponent;
+  get wrapperDom() {
+    return this.#wrapperDom;
+  }
+  constructor() {
+    this.#initWrapperDom();
+  }
+    #initWrapperDom() {
+    const notificationWrapper = document.createElement('jb-notification-wrapper') as JBNotificationWrapperWebComponent;
+    document.body.appendChild(notificationWrapper);
+    this.#wrapperDom = notificationWrapper;
+  }
+    #createMessageDom(params:NewNotificationProps) {
+      const {title, type, desc} = params;
+      const notificationDom = document.createElement('jb-notification') as JBNotificationWebComponent;
+      notificationDom.title = title;
+      if(type){ notificationDom.type = type};
+      if(desc){notificationDom.description = desc};
+      notificationDom.addEventListener("close", this.onNotificationClose.bind(this));
+      return notificationDom;
+    }
+    new(params:NewNotificationProps) {
+      const {title, type, desc} = params;
+      const dom = this.#createMessageDom({title, type, desc});
+      //TODO: change it to new web component wrapper with managerial ability
+      this.wrapperDom?.appendChild(dom);
+      dom.show();
+    }
+    onNotificationClose(e) {
+      this.wrapperDom.removeChild(e.target);
+    }
+}
+
+export type NewNotificationProps = {
+  title:string,
+  type?:NotificationType,
+  desc?:string
+}
